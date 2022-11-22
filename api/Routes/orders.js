07a5938f06ce742d4097cbc0dbe9c82e033193d1
Eakudompong Chanoknan <ceguantz96@outlook.com>
2022-11-22 14:40:13 +0900

    Update order respone & error handling

--- api/Routes/orders.js
+++ api/Routes/orders.js
@@ -4,7 +4,15 @@ const Product = require("../Model/Product");
 
 router.get("/", async (req, res) => {
   try {
-    res.status(200).send(await Order.find());
+    const result = await Order.find();
+    const doc = result.map((doc) => {
+      return {
+        method: "GET",
+        url: "http://localhost:3000/orders/" + doc._id,
+        body: { _id: doc._id, product: doc.product, quantity: doc.quantity },
+      };
+    });
+    res.status(200).send(doc);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -13,9 +21,8 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const productId = req.body.productId;
-
     const product = await Product.findById(productId).catch((err) => {
-      res.status(404).json(err);
+      res.status(404).json({ message: "Product not Found", erorr: err });
     });
 
     const order = new Order({
@@ -23,19 +30,37 @@ router.post("/", async (req, res) => {
       quantity: req.body.quantity,
     });
 
-    await order.save();
+    const newOrder = await order.save();
 
-    res.status(201).json(order);
+    res.status(201).json({
+      Method: "POST",
+      url: "http://localhost:3000/orders",
+      body: {
+        _id: newOrder._id,
+        product: newOrder.product,
+        quantity: newOrder.quantity,
+      },
+    });
   } catch (err) {
-    console.log(err);
-    res.status(500).json("Something went Wrong");
+    let error = null ? err : "Product not Found";
+
+    res.status(500).json({ message: "Something went Wrong", error: error });
   }
 });
 
 router.get("/:orderId", async (req, res) => {
   try {
     const orderId = req.params.orderId;
-    res.status(200).json(await Order.findById(orderId));
+    const order = await Order.findById(orderId);
+    res.status(200).json({
+      Method: "GET",
+      url: "http://localhost:3000/orders",
+      body: {
+        _id: order._id,
+        product: order.product,
+        quantity: order.quantity,
+      },
+    });
   } catch (err) {
     res.status(404);
   }
@@ -45,10 +70,10 @@ router.delete("/:orderId", async (req, res) => {
   try {
     const orderId = req.params.orderId;
     const removed = await Order.findByIdAndRemove(orderId);
-
-    res.status(200).json(removed);
+    if (removed == null) res.status(404).json({ message: "Order not Found" });
+    res.status(200).json({ message: "Removed Order", order: removed });
   } catch (err) {
-    res.status(404).json(err);
+    res.status(500).json(err);
   }
 });
 
