5c736423299e1b1a191660c416232d63d2f4aee2
Eakudompong Chanoknan <ceguantz96@outlook.com>
2022-12-12 12:26:34 +0900

    add email validatiob

--- api/Model/User.js
+++ api/Model/User.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   username: { type: String, required: true },
-  email: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match:
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+  },
   password: { type: String, required: true },
 });
 
